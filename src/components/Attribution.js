3acd98d47890bdaa4ed9cc2b60c27ec6ef83d3bc
Kumar Snehanshu <kumarsnehanshu2022@vitbhopal.ac.in>
2025-05-31 21:02:02 +0530

    first commit

---
+++ src/components/Attribution.js
@@ -0,0 +1,7 @@
+export default function Attribution() {
+   return (
+      <footer className="attribution">
+         <p>developed by <a href="https://none.it" target="_blank" rel="noreferrer">Snehanshu</a></p>
+      </footer>
+   );
+}
